06d44c3ad1d98de841f123b003cb790dda03b719
Quvonchbek Murodov <quvonchbe05@gmail.com>
2023-03-10 21:26:10 +0500

    v3

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','unimart.up.railway.app']
 
 
 # Application definition
